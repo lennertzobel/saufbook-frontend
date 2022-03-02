9ed4aac6e6e34262c0228ff21616b08981973eca
lennertzobel <lennert.zobel@gmail.com>
2022-03-02 15:47:24 +0100

    Reformatted code with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     fontSize: {
-      '3.5': '.875rem',
-      '4': '1rem',
-      '5': '1.25rem',
-      '7': ['1.75rem', {
-        letterSpacing: '-.03em'
-      }],
+      3.5: ".875rem",
+      4: "1rem",
+      5: "1.25rem",
+      7: [
+        "1.75rem",
+        {
+          letterSpacing: "-.03em",
+        },
+      ],
     },
     extend: {
       fontFamily: {
-        'sans': ['Poppins', ...defaultTheme.fontFamily.sans]
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
       dropShadow: {
-        'nav': '0px 0px 16px rgba(0,0,0,0.1)'
-      }
+        nav: "0px 0px 16px rgba(0,0,0,0.1)",
+      },
     },
   },
   plugins: [],
-}
+};
